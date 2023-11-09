20fa6c3cd6677c4470d5eeac884e8dec90851b18
vedangit <vedangithokal@gmail.com>
2023-11-09 12:53:30 +0530

    timefucn

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
-import speech
+import speech as s
+import datetime 
 
+if __name__ == "__main__":
+    while True:
+        query = s.takeCommand().lower()
+ 
+       
+        if 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            print("Sir, the time is {}".format(strTime))
+            s.speak("Sir, the time is {}".format(strTime))
+ 
 
+         
+        if  'leave' in query:
+            print("Goodbye Sir. Have a nice day.")
+            s.speak("Goodbye Sir. Have a nice day.")
+            break
+ 
 
