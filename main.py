edab709328557abbaac994d91252d9e238df32f5
vedangit <vedangithokal@gmail.com>
2023-11-09 18:46:18 +0530

    fury

--- main.py
+++ main.py
@@ -8,12 +8,21 @@ if __name__ == "__main__":
        
         if 'the time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
-            print("Sir, the time is {}".format(strTime))
-            s.speak("Sir, the time is {}".format(strTime))
+            print("The time is {}".format(strTime))
+            s.speak("The time is {}".format(strTime))
  
+    
+        elif 'nick'or 'nicholas' or 'joseph' in query:
+            nick1 = "Everybody calls me Fury. not nicholas, not joseph, not nick. just. fury."
+            print(nick1)
+            s.speak(nick1)
+            
+        elif 'what does your mom call you' or 'what do you call your mom' or 'what would your kids call you'  in query:
+            nick2 = "fury"
+            print(nick2)
+            s.speak(nick2)
 
-         
-        if  'leave' in query:
+        elif  'leave' in query:
             print("Goodbye Sir. Have a nice day.")
             s.speak("Goodbye Sir. Have a nice day.")
             break
